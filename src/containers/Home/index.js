5a73bd147e53696d470062b73c6793e1f440a82f
superdev808 <drose6307@gmail.com>
2024-01-09 06:05:10 -0500

    Update input event

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
+
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Spinner from "react-bootstrap/Spinner";
 import SummaryInfo from "../../components/SummaryInfo";
 import { fetchSchoolsList } from "../../utils/api";
-import Spinner from "react-bootstrap/Spinner";
 import SearchInput from "../../components/SearchBox";
 
 export default function Home() {
   const [schools, setSchools] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [total, setTotal] = useState(0);
-  const [pageNum, setPageNum] = useState(0);
+  const [total, setTotal] = useState(1);
+  const [pageNum, setPageNum] = useState(1);
   const [queryString, setQueryString] = useState("");
   const pageSize = 10;
   const isMounted = useRef(false);
@@ -22,6 +23,14 @@ export default function Home() {
       schools.length < total
     ) {
       setPageNum(pageNum + 1);
+    }
+  };
+
+  const handleInput = (inputValue) => {
+    if (queryString !== inputValue) {
+      setQueryString(inputValue);
+      setSchools([]);
+      setPageNum(1);
     }
   };
 
@@ -53,19 +62,21 @@ export default function Home() {
 
   return (
     <Container>
-      {total > 0 && (
-        <Row className="mt-4">
-          <Col md={3} sm={12} className="d-flex align-items-center">
-            {/* <p className="h6">{total} schools</p> */}
-            <p className="h5">Transfer University Profiles</p>
-          </Col>
-					<Col md={{ span: 6, offset: 3 }} sm={12} className="d-flex align-items-center">
-						<SearchInput setQueryString={setQueryString} />
-					</Col>
-        </Row>
-      )}
+      <Row className="mt-4">
+        <Col md={3} sm={12} className="d-flex align-items-center">
+          {/* <p className="h6">{total} schools</p> */}
+          <p className="h5">Transfer University Profiles</p>
+        </Col>
+        <Col
+          md={{ span: 6, offset: 3 }}
+          sm={12}
+          className="d-flex align-items-center"
+        >
+          <SearchInput onInput={handleInput} />
+        </Col>
+      </Row>
       {schools.map((school) => (
-        <Row key={school.id}>
+        <Row key={`ui-${school.id}`}>
           <Col>
             <SummaryInfo {...school} />
           </Col>
